ac83b261d23ddceb8359d13c21058c61bb0ba6a3
seylu <98249191+seyLu@users.noreply.github.com>
2024-08-15 07:13:15 +0800

    feat (api): del route.go

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/seyLu/gofiftyville/internal/api"
 )
 
 func StartServer(domain string, port int) {
 	router := gin.Default()
 
-	router.GET("crime-scene-reports", api.GetCrimeSceneReports())
+	router.GET("crime-scene-reports", getCrimeSceneReports)
 
 	host := fmt.Sprintf("%v:%v", domain, port)
 	router.Run(host)
+}
+
+func getCrimeSceneReports(c *gin.Context) {
+	fmt.Println(c.Request.Body)
 }
