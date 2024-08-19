f844c39ee561f101bcd84b4442489976f7c74ff0
seylu <98249191+seyLu@users.noreply.github.com>
2024-08-20 04:21:07 +0800

    feat (api): leaner people table query

--- internal/model/person.go
+++ internal/model/person.go
@@ -17,8 +17,12 @@ type Person struct {
 }
 
 type PersonBankAccount struct {
-	Person
-	BankAccount
+	Name           string
+	PhoneNumber    null.String
+	PassportNumber null.Int
+	LicensePlate   null.String
+	AccountNumber  int
+	CreationYear   int
 }
 
 type PeopleFilter struct {
@@ -31,11 +35,11 @@ func People(f PeopleFilter) ([]PersonBankAccount, error) {
 	var filters []string
 	query := `
 		SELECT
-			id, name, phone_number, passport_number, license_plate,
-			account_number, person_id, creation_year
-		FROM people
-		INNER JOIN bank_accounts
-			ON people.id=bank_accounts.person_id
+			p.name, p.phone_number, p.passport_number, p.license_plate,
+			b.account_number, b.creation_year
+		FROM people AS p
+		INNER JOIN bank_accounts AS b
+			ON b.person_id=p.id
 	`
 	args := []any{}
 
@@ -75,7 +79,10 @@ func People(f PeopleFilter) ([]PersonBankAccount, error) {
 	var accounts []PersonBankAccount
 	for rows.Next() {
 		var account PersonBankAccount
-		if err := rows.Scan(&account.ID, &account.Name, &account.PhoneNumber, &account.PassportNumber, &account.LicensePlate, &account.AccountNumber, &account.PersonId, &account.CreationYear); err != nil {
+		if err := rows.Scan(
+			&account.Name, &account.PhoneNumber, &account.PassportNumber, &account.LicensePlate,
+			&account.AccountNumber, &account.CreationYear,
+		); err != nil {
 			return nil, fmt.Errorf("People %+v: %w", f, err)
 		}
 		accounts = append(accounts, account)
