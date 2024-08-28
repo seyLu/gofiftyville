303aaca636acb8db318a84aef6f446dd01463b15
seylu <98249191+seyLu@users.noreply.github.com>
2024-08-28 09:01:05 +0800

    feat (test): add more /interviews test

--- internal/controller/interviews-controller_test.go
+++ internal/controller/interviews-controller_test.go
@@ -26,3 +26,41 @@ func TestGetInterviews(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 }
+
+func TestGetInterviewsBadDate(t *testing.T) {
+	store.SetupTestDB()
+	defer func() {
+		if err := store.CloseDB(); err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+	}()
+
+	r := router.SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/interviews", nil)
+	q := req.URL.Query()
+	q.Add("date", "January 20, 202")
+	req.URL.RawQuery = q.Encode()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 422, w.Code)
+}
+
+func TestGetInterviewsGoodDate(t *testing.T) {
+	store.SetupTestDB()
+	defer func() {
+		if err := store.CloseDB(); err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+	}()
+
+	r := router.SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/interviews", nil)
+	q := req.URL.Query()
+	q.Add("date", "January 20, 2021")
+	req.URL.RawQuery = q.Encode()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
